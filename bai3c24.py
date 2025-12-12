49743708f28ad0af99c0e5149aaf7245bdbec9a6
gbao12806-create <gbao12806@gmail.com>
2025-12-12 21:15:26 +0700

    Update bai3c24.py

--- bai3c24.py
+++ bai3c24.py
@@ -1,5 +1,10 @@
+print("sinh vien : Ho Gia Bao") 
+print("ma so sv :245751030110059")
+print("#############################")
+
 s = input("Nhap cau: ")
 upper = sum(c.isupper() for c in s)
 lower = sum(c.islower() for c in s)
 print("Chu hoa:", upper)
 print("Chu thuong:", lower)
+
