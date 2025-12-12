ebf529bcac9c618b8537fe9cb48f9fd0a8c3e3d8
gbao12806-create <gbao12806@gmail.com>
2025-12-12 21:14:42 +0700

    Update bai3c21.py

--- bai3c21.py
+++ bai3c21.py
@@ -1,6 +1,11 @@
+print("sinh vien : Ho Gia Bao") 
+print("ma so sv :245751030110059")
+print("#############################")
+
 lst = input("Nhap cac so nhi phan 4 chu so, cach boi dau phay: ").split(',')
 res = []
 for b in lst:
 if int(b, 2) % 5 == 0:
 res.append(b)
 print(','.join(res))
+
