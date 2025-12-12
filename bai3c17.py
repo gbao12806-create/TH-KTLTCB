94b0839f4c253fe600a2e71112656365b809ccdc
gbao12806-create <gbao12806@gmail.com>
2025-12-12 21:13:48 +0700

    Update bai3c17.py

--- bai3c17.py
+++ bai3c17.py
@@ -1,5 +1,10 @@
+print("sinh vien : Ho Gia Bao") 
+print("ma so sv :245751030110059")
+print("#############################")
+
 n = int(input("Nhap n: "))
 for i in range(1, n):
 tong = sum([j for j in range(1, i) if i % j == 0])
 if tong > i:
 print(i)
+
