72e199a280d5eddabc396c0c7a4128f0ad51cbe1
gbao12806-create <gbao12806@gmail.com>
2025-12-12 21:15:13 +0700

    Update bai3c23.py

--- bai3c23.py
+++ bai3c23.py
@@ -1,5 +1,10 @@
+print("sinh vien : Ho Gia Bao") 
+print("ma so sv :245751030110059")
+print("#############################")
+
 s = input("Nhap cau: ")
 letters = sum(c.isalpha() for c in s)
 digits = sum(c.isdigit() for c in s)
 print("So chu cai la:", letters)
 print("So chu so la:", digits)
+
