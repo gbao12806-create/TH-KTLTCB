823cbe8f25ec5874ee63aaeadca02579d7ba4c75
gbao12806-create <gbao12806@gmail.com>
2025-12-12 21:13:30 +0700

    Update bai3c16.py

--- bai3c16.py
+++ bai3c16.py
@@ -1,3 +1,8 @@
+print("sinh vien : Ho Gia Bao") 
+print("ma so sv :245751030110059")
+print("#############################")
+
 s = input("Nhap cac so nhi phan cach nhau boi dau phay: ").split(',')
 for item in s:
 print(item)
+
