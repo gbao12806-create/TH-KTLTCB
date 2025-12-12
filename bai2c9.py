c7afabd01776b55e923a6a7d84700fca06cdd957
gbao12806-create <gbao12806@gmail.com>
2025-12-12 21:12:07 +0700

    Update bai2c9.py

--- bai2c9.py
+++ bai2c9.py
@@ -1,6 +1,11 @@
+print("sinh vien : Ho Gia Bao") 
+print("ma so sv :245751030110059")
+print("#############################")
+
 def calc(a,b,op):
         if op == '+': return a+b
         if op == '-': return a-b
         if op == '*': return a*b
         if op == '/': return a/b
     print(calc(10,5,'+'))
+
