3e5b267ebc4e2e71f7a734c2299768bac2b0879c
gbao12806-create <gbao12806@gmail.com>
2025-12-12 21:14:02 +0700

    Update bai3c18.py

--- bai3c18.py
+++ bai3c18.py
@@ -1,5 +1,10 @@
+print("sinh vien : Ho Gia Bao") 
+print("ma so sv :245751030110059")
+print("#############################")
+
 n = int(input("Nhap n: "))
 fib = [0, 1]
 while fib[-1] + fib[-2] < n:
 fib.append(fib[-1] + fib[-2])
 print(fib)
+
