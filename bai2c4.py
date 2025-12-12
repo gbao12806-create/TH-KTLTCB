27a3f2e3fb566dcfe5ba6858f746835b748e5e9c
gbao12806-create <gbao12806@gmail.com>
2025-12-12 21:11:13 +0700

    Update bai2c4.py

--- bai2c4.py
+++ bai2c4.py
@@ -1,6 +1,11 @@
+print("sinh vien : Ho Gia Bao") 
+print("ma so sv :245751030110059")
+print("#############################")
+
 x = 10
     def show():
         x = 20
         print("x trong ham =", x)
     show()
     print("x ngoai ham =", x)
+
