183f7b355cfe2779dd6a3aa46e95c2fd06670b0a
gbao12806-create <gbao12806@gmail.com>
2025-12-12 21:10:35 +0700

    Update bai2c11.py

--- bai2c11.py
+++ bai2c11.py
@@ -1,6 +1,11 @@
- p = 1000000
+print("sinh vien : Ho Gia Bao") 
+print("ma so sv :245751030110059")
+print("#############################")
+
+p = 1000000
     rate = 0.06
     years = 3
     for _ in range(years):
         p += p*rate
     print("So tien sau 3 nam:", p)
+
