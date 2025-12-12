130363393121d01687ef17735c110ce6ea665122
gbao12806-create <gbao12806@gmail.com>
2025-12-12 21:10:22 +0700

    Update bai2c10.py

--- bai2c10.py
+++ bai2c10.py
@@ -1,5 +1,10 @@
+print("sinh vien : Ho Gia Bao") 
+print("ma so sv :245751030110059")
+print("#############################")
+
 import math
     r = 5
     cv = 2*math.pi*r
     dt = math.pi*r*r
     print("Chu vi =",cv," Dien tich =",dt)
+
