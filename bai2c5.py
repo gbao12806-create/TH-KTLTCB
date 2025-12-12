88153ac59754d5e9bab91e0593f537934960bef1
gbao12806-create <gbao12806@gmail.com>
2025-12-12 21:11:25 +0700

    Update bai2c5.py

--- bai2c5.py
+++ bai2c5.py
@@ -1,6 +1,11 @@
+print("sinh vien : Ho Gia Bao") 
+print("ma so sv :245751030110059")
+print("#############################")
+
 x = 5
     def change():
         global x
         x = 10
     change()
     print("x =", x)
+
