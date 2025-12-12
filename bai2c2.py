663f442741b9823a33a967869e638af0bd478806
gbao12806-create <gbao12806@gmail.com>
2025-12-12 21:10:52 +0700

    Update bai2c2.py

--- bai2c2.py
+++ bai2c2.py
@@ -1,4 +1,9 @@
+print("sinh vien : Ho Gia Bao") 
+print("ma so sv :245751030110059")
+print("#############################")
+
 def sum(a, b):
         return a + b
         c=sum(4,5);
     print("Tong cua 4 va 5=",+str(c))
+
