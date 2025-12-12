5ba7f2848944fdf844a48a379561eb60ee107361
gbao12806-create <gbao12806@gmail.com>
2025-12-12 21:11:53 +0700

    Update bai2c7.py

--- bai2c7.py
+++ bai2c7.py
@@ -1,3 +1,8 @@
+print("sinh vien : Ho Gia Bao") 
+print("ma so sv :245751030110059")
+print("#############################")
+
 def even_odd(n):
         return "Chan" if n % 2 == 0 else "Le"
     print("10 la so:", even_odd(10))
+
