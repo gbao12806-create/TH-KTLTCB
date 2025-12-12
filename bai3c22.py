4ab5fd7d13e53e0b00b6f501fe362704a7641654
gbao12806-create <gbao12806@gmail.com>
2025-12-12 21:15:01 +0700

    Update bai3c22.py

--- bai3c22.py
+++ bai3c22.py
@@ -1,5 +1,10 @@
+print("sinh vien : Ho Gia Bao") 
+print("ma so sv :245751030110059")
+print("#############################")
+
 res = []
 for num in range(1000, 3001):
 if all(int(c) % 2 == 0 for c in str(num)):
 res.append(str(num))
 print(','.join(res))
+
