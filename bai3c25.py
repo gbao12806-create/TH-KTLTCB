096883b8bcedebda2caf809b4a3706c2fab664f3
gbao12806-create <gbao12806@gmail.com>
2025-12-12 21:15:43 +0700

    Update bai3c25.py

--- bai3c25.py
+++ bai3c25.py
@@ -1,3 +1,8 @@
+print("sinh vien : Ho Gia Bao") 
+print("ma so sv :245751030110059")
+print("#############################")
+
 lst = list(map(int, input("Nhap danh sach so, cach nhau boi dau phay: ").split(',')))
 res = [x for x in lst if x % 2 == 1]
 print(res)
+
