dfe8885293f945f1c32ef24b086e2a636056892a
gbao12806-create <gbao12806@gmail.com>
2025-12-12 21:15:56 +0700

    Update bai3c26.py

--- bai3c26.py
+++ bai3c26.py
@@ -1,3 +1,7 @@
+print("sinh vien : Ho Gia Bao") 
+print("ma so sv :245751030110059")
+print("#############################")
+
 tong = 0
 print("Nhap cac giao dich (D/W so_tien), nhap rong de ket thuc:")
 while True:
@@ -11,3 +15,4 @@ tong += v
 elif t == 'W':
 tong -= v
 print(tong)
+
