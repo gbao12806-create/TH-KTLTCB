32e0e900b4d0bd693afa795e07dcbdf5ec6164e7
gbao12806-create <gbao12806@gmail.com>
2025-12-12 21:11:38 +0700

    Update bai2c6.py

--- bai2c6.py
+++ bai2c6.py
@@ -1,3 +1,8 @@
- def demo(*args):
+print("sinh vien : Ho Gia Bao") 
+print("ma so sv :245751030110059")
+print("#############################")
+
+def demo(*args):
         print("So tham so:", len(args), "- Gia tri:", args)
     demo(1, 2, 3, "python")
+
