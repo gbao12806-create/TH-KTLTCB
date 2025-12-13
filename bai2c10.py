e961f9aaf52cb15f28cf1787657256a0753e136b
gbao12806-create <gbao12806@gmail.com>
2025-12-13 08:30:30 +0700

    Update bai2c10.py

--- bai2c10.py
+++ bai2c10.py
@@ -3,8 +3,9 @@ print("ma so sv :245751030110059")
 print("#############################")
 
 import math
-    r = 5
-    cv = 2*math.pi*r
-    dt = math.pi*r*r
-    print("Chu vi =",cv," Dien tich =",dt)
+r = 5
+cv = 2*math.pi*r
+dt = math.pi*r*r
+print("Chu vi =",cv," Dien tich =",dt)
+
 
