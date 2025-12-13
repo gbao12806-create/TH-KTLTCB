aff09500cffb62fcd80d78d3a29b75d8b9693af4
gbao12806-create <gbao12806@gmail.com>
2025-12-13 08:27:38 +0700

    Update bai2c2.py

--- bai2c2.py
+++ bai2c2.py
@@ -3,7 +3,7 @@ print("ma so sv :245751030110059")
 print("#############################")
 
 def sum(a, b):
-        return a + b
-        c=sum(4,5);
-    print("Tong cua 4 va 5=",+str(c))
+    return a + b
+c = sum(4, 5)
+print("Tong cua 4 va 5 = " + str(c))
 
